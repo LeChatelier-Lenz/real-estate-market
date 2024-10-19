138eaa1c16fe53a6e2103de5a8ee2c6c5f63a351
LeChatelier-Lenz <lechate0222@gmail.com>
2024-10-20 00:05:18 +0800

    前端区块链网络&合约配置

---
+++ house-change/src/utils/contracts.ts
@@ -0,0 +1,28 @@
+import Addresses from './contract-addresses.json'
+import HousieCoin from './abis/HousieCoin.json'
+import TokenSale from './abis/TokenSale.json'
+import RealEstate from './abis/RealEstate.json'
+import RESwap from './abis/RESwap.json'
+
+const Web3 = require('web3');
+
+//@ts-ignore
+//创建web3实例
+let web3 = new Web3(window.web3.currentProvider);
+
+//修改地址为部署的合约地址
+const HousieCoinAddress = Addresses.HousieCoin
+const HousieCoinABI = HousieCoin.abi
+const TokenSaleAddress = Addresses.TokenSale
+const TokenSaleABI = TokenSale.abi
+const RealEstateAddress = Addresses.RealEstate
+const RealEstateABI = RealEstate.abi
+const RESwapAddress = Addresses.RESwap
+const RESwapABI = RESwap.abi
+
+const HouseCoinContract = new web3.eth.Contract(HousieCoinABI,HousieCoinAddress);
+const TokenSaleContract = new web3.eth.Contract(TokenSaleABI,TokenSaleAddress);
+const RealEstateContract = new web3.eth.Contract(RealEstateABI,RealEstateAddress);
+const RESwapContract = new web3.eht.Contract(RESwapABI,RESwapAddress);
+
+export {web3,HouseCoinContract,TokenSaleContract,RealEstateContract,RESwapContract}
